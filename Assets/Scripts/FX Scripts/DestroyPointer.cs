5f34c7d26ebb8ed109eeafad055d151687b13dcb
harmonymiracle <1459078670@qq.com>
2018-02-28 20:16:06 +0800

    Complete the Health and Power UI

---
+++ Assets/Scripts/FX Scripts/DestroyPointer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPointer : MonoBehaviour {
+
+	private Transform player;
+	
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+	}
+	
+	
+	void Update () {
+
+		if (Vector3.Distance (player.transform.position, transform.position) <= 1.0f) {
+			Destroy (gameObject);
+		}
+	}
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttack : MonoBehaviour {
+
+	public Image fillWaitImage_1;
+	public Image fillWaitImage_2;
+	public Image fillWaitImage_3;
+	public Image fillWaitImage_4;
+	public Image fillWaitImage_5;
+	public Image fillWaitImage_6;
+
+	private int[] fadeImages = new int[] { 0, 0, 0, 0, 0, 0 };
+
+	private Animator anim;
+	private bool canAttack = true;
+
+	private PlayerMove playerMove;
+
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+		playerMove = GetComponent <PlayerMove> ();
+
+	}
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		if (!anim.IsInTransition (0) && anim.GetCurrentAnimatorStateInfo(0).IsName ("Stand")) {
+			canAttack = true;
+		} else {
+			canAttack = false;
+		}
+
+		// CheckToFade ();
+		CheckInput ();
+	}
+
+
+
+	void CheckInput () {
+
+		// 0 - 6 mean 6 different skills
+		if (anim.GetInteger ("Atk") == 0) {
+			playerMove.FinishedMovement = false;
+
+			if (!anim.IsInTransition (0) && anim.GetCurrentAnimatorStateInfo(0).IsName("Stand")) {
+				playerMove.FinishedMovement = true;
+			}
+		}
+
+
+	}
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
